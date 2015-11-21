600bccf4e440980338adb991f1ade6308dc6c880
Colin Fahrion <colin.fahrion@gmail.com>
2015-11-20 17:04:16 -0800

    Started process. Got News working again!

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,6 +9,28 @@ angular.module('myApp')
 	$scope.item = $data.items[$scope.myNavigator.getCurrentPage().options.index];
 })
 
+.controller("NewsCtrl", function($http, $scope) {
+
+    $scope.init = function() {
+
+      $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://fetus.ucsfmedicalcenter.org/feed/" } })
+        .success(function(data) {
+                $scope.rssTitle = data.responseData.feed.title;
+                $scope.rssUrl = data.responseData.feed.feedUrl;
+                $scope.rssSiteUrl = data.responseData.feed.link;
+                $scope.entries = data.responseData.feed.entries;
+                window.localStorage["entries"] = JSON.stringify(data.responseData.feed.entries);
+            })
+            .error(function(data) {
+                console.log("ERROR: " + data);
+                if(window.localStorage["entries"] !== undefined) {
+                    $scope.entries = JSON.parse(window.localStorage["entries"]);
+                }
+            });
+    }
+
+})
+
 .factory('$data',function() {
 		var data = {};
 
