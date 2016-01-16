9522093bd0ac2d8bcce6595766d768ee011563e5
Colin Fahrion <colin.fahrion@gmail.com>
2016-01-15 16:51:31 -0800

    new commit with icons and everything

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -3,6 +3,7 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
         "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "pluginId": "cordova-plugin-inappbrowser",
         "clobbers": [
             "cordova.InAppBrowser.open",
             "window.open"
