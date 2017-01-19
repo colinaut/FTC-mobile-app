2eef0a601f7893e95d48411c0a5cfd9b2ec565be
Colin Fahrion <colin.fahrion@gmail.com>
2017-01-18 22:52:50 -0800

    Set up Gulp Less

--- gulpfile.js
+++ gulpfile.js
@@ -117,6 +117,20 @@ gulp.task('prepare-cordova', function() {
     .pipe($.shell(['cordova prepare'], {cwd: __dirname}));
 });
 
+////////////////////
+// Less
+////////////////////
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('less', function () {
+  return gulp.src('www/style/*.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ]
+    }))
+    .pipe(gulp.dest('www/style'));
+});
+
 // utils
 function plumber() {
   return $.plumber({errorHandler: $.notify.onError()});
